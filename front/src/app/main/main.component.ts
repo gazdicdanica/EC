40bcae1256668e28230063bb6c6d72817c36c25f
Danica Gazdić <danicagazdic@gmail.com>
2024-04-27 21:14:48 +0200

    course item design

--- front/src/app/main/main.component.ts
+++ front/src/app/main/main.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class MainComponent {
 
+  edu_list = [
+    {"name": "Math", "color": "#1976D2"},   // Blue
+    {"name": "History", "color": "#388E3C"}, // Green
+    {"name": "Programming", "color": "#0277BD"}, // Light Blue
+    {"name": "Physics", "color": "#689F38"}, // Light Green
+    {"name": "Chemistry", "color": "#0097A7"}, // Cyan
+    {"name": "Biology", "color": "#00796B"}, // Teal
+    {"name": "Geography", "color": "#004D40"}, // Dark Cyan
+    {"name": "Literature", "color": "#37474F"}, // Blue Grey
+    {"name": "Art", "color": "#455A64"} // Blue Grey Darken 2
+];
 }
